1e3581cc88db7b88ec69918b54ec653624525bb9
royMatar <116262739+royMatar@users.noreply.github.com>
2023-04-28 06:16:15 +0300

    Add files via upload

---
+++ caesarCipher/index.js
@@ -0,0 +1,72 @@
+// Given a phrase, substitute each character by shifting it up
+// the alphabet by a given integer. If necessary, the shifting
+// should wrap around back to the beginning of the alphabet.
+// Make sure the function only shifts letters.
+// --- Examples
+// caeserCipher("abcd", 1) === "bcde";
+// caeserCipher("yz", 1) === "za";
+// caeserCipher("abcd", 100) === "wxyz";
+// caeserCipher("gurer ner 9 qbtf!", 13) === "there are 9 dogs!"
+
+function caesarCipher(str, shift) {
+  // Define an array of letters in the alphabet
+  const alphabetArr = ["abcdefghijklmnopqrstuvwxyz"].split("");
+  // Initialize an empty string to hold the result
+  let res = "";
+
+  // Loop through each character in the input strin
+  for (let i = 0; i < str.length; i++) {
+    // Get the current character and its index in the alphabet array
+    const char = str[i];
+    const idx = alphabetArr.indexOf(char);
+    // If the character is not in the alphabet (e.g. a space or punctuation), add it to the result as is
+    if (idx === -1) {
+      res += char;
+      continue;
+    }
+    // Calculate the new index of the character after shifting by the specified amount
+    const encodedIdx = (idx + shift) % 26;
+    // Add the shifted character to the result
+    res += alphabetArr[encodedIdx];
+  }
+  // Return the resulting encoded string
+  return res;
+}
+// _________ _______  _______ _________   _______  _______  _______  _______  _______
+// \__   __/(  ____ \(  ____ \\__   __/  (  ____ \(  ___  )(  ____ \(  ____ \(  ____ \
+//    ) (   | (    \/| (    \/   ) (     | (    \/| (   ) || (    \/| (    \/| (    \/
+//    | |   | (__    | (_____    | |     | |      | (___) || (_____ | (__    | (_____
+//    | |   |  __)   (_____  )   | |     | |      |  ___  |(_____  )|  __)   (_____  )
+//    | |   | (            ) |   | |     | |      | (   ) |      ) || (            ) |
+//    | |   | (____/\/\____) |   | |     | (____/\| )   ( |/\____) || (____/\/\____) |
+//    )_(   (_______/\_______)   )_(     (_______/|/     \|\_______)(_______/\_______)
+//                             ____       _
+//                             |  _ \     | |
+//                             | |_) | ___| | _____      __
+//                             |  _ < / _ \ |/ _ \ \ /\ / /
+//                             | |_) |  __/ | (_) \ V  V /
+//                             |____/ \___|_|\___/ \_/\_/
+//                         ______ ______ ______ ______ ______
+//                         |______|______|______|______|______|
+
+//                          ______ ______ ______ ______ ______
+//                         |______|______|______|______|______|
+
+//                          ______ ______ ______ ______ ______
+//                         |______|______|______|______|______|
+
+mocha.setup("bdd");
+const { assert } = chai;
+
+describe("caesarCipher()", () => {
+  it("shifts letters correctly.", () => {
+    assert.equal(caesarCipher("abcd", 1), "bcde");
+    assert.equal(caesarCipher("yz", 1), "za");
+    assert.equal(caesarCipher("abcd", 100), "wxyz");
+  });
+  it("does not shift digits or other characters such as '!'.", () => {
+    assert.equal(caesarCipher("gurer ner 9 qbtf!", 13), "there are 9 dogs!");
+  });
+});
+
+mocha.run();
---
+++ sorting/index.js
@@ -0,0 +1,82 @@
+// Implement a swap helper function that we will use in both BS and SS
+function swap(arr, i, j) {
+  let temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+}
+
+//bubbleSort works by having sorted data accumulate at end of array
+
+//Time complexity O(N^2)
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        swap(arr, j, j + 1);
+      }
+    }
+  }
+  return arr;
+}
+
+//selectionSort works by having sorted data accumulate at start of array
+function selectionSort(arr) {
+  for (let i = 0; i, arr.length; i++) {
+    let swapIdx = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[swapIdx]) {
+        swapIdx = j;
+      }
+    }
+    swap(arr, i, swapIdx);
+  }
+  return arr;
+}
+
+// _________ _______  _______ _________   _______  _______  _______  _______  _______
+// \__   __/(  ____ \(  ____ \\__   __/  (  ____ \(  ___  )(  ____ \(  ____ \(  ____ \
+//    ) (   | (    \/| (    \/   ) (     | (    \/| (   ) || (    \/| (    \/| (    \/
+//    | |   | (__    | (_____    | |     | |      | (___) || (_____ | (__    | (_____
+//    | |   |  __)   (_____  )   | |     | |      |  ___  |(_____  )|  __)   (_____  )
+//    | |   | (            ) |   | |     | |      | (   ) |      ) || (            ) |
+//    | |   | (____/\/\____) |   | |     | (____/\| )   ( |/\____) || (____/\/\____) |
+//    )_(   (_______/\_______)   )_(     (_______/|/     \|\_______)(_______/\_______)
+//                             ____       _
+//                             |  _ \     | |
+//                             | |_) | ___| | _____      __
+//                             |  _ < / _ \ |/ _ \ \ /\ / /
+//                             | |_) |  __/ | (_) \ V  V /
+//                             |____/ \___|_|\___/ \_/\_/
+//                         ______ ______ ______ ______ ______
+//                         |______|______|______|______|______|
+
+//                          ______ ______ ______ ______ ______
+//                         |______|______|______|______|______|
+
+//                          ______ ______ ______ ______ ______
+//                         |______|______|______|______|______|
+
+mocha.setup("bdd");
+const { assert } = chai;
+
+describe("swap()", () => {
+  it("swaps values in an array when provided with 2 indexes.", () => {
+    const arr = [13, 2, 4];
+    swap(arr, 0, 1);
+    assert.deepEqual(arr, [2, 13, 4]);
+  });
+});
+
+describe("Bubble Sort", () => {
+  it("sorts an array", () => {
+    assert.deepEqual(bubbleSort([5, 1, 3, 7, 6, 2, 4]), [1, 2, 3, 4, 5, 6, 7]);
+  });
+});
+
+describe("Selection Sort", () => {
+  it("sorts an array", () => {
+    assert.deepEqual(selectionSort([5, 1, 3, 2, 4]), [1, 2, 3, 4, 5]);
+  });
+});
+
+mocha.run();
